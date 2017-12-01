8301423adb2fc998aab20aaaa390f4b8818d0fc0
Katie Atwell <katieatwell@outlook.com>
2017-12-01 04:13:13 +0000

    Final Submission

--- nodeHomework/keys.js
+++ nodeHomework/keys.js
@@ -1,15 +1,13 @@
-console.log('this is loaded');
-
-// exports.twitterKeys = ({
-//     consumer_key: 'FZPzKxDTk9XTtipx2pI9jgIXQ',
-//     consumer_secret: 'Qq1BWNAFpyYCbHcBGM11OxCTEwaPVetLZm5VYfwifp5sBp635L',
-//     access_token_key: '935555131592052736-ZIiyEkOBaJahCSJGd7nE5pjnP88ScvF',
-//     access_token_secret: 'zzuhwLlja18bi2JhY3RESKUwnQ6qdyVskOCFDntt1QgHW',
-// });
+var twitterKeys = ({
+    consumer_key: 'FZPzKxDTk9XTtipx2pI9jgIXQ',
+    consumer_secret: 'Qq1BWNAFpyYCbHcBGM11OxCTEwaPVetLZm5VYfwifp5sBp635L',
+    access_token_key: '935555131592052736-ZIiyEkOBaJahCSJGd7nE5pjnP88ScvF',
+    access_token_secret: 'zzuhwLlja18bi2JhY3RESKUwnQ6qdyVskOCFDntt1QgHW',
+});
 
 var spotify = ({
     id: "e8d8988be74f47bfa2b93b966242aa83",
     secret: "2aaec3dabbea4b5abb257862192e4e80",
 });
 
-module.exports = spotify;
+module.exports = { twitterKeys: twitterKeys, spotify: spotify };
--- nodeHomework/liri.js
+++ nodeHomework/liri.js
@@ -1,34 +1,30 @@
 //Global Variables
 var request = require("request");
 var keys = require("./keys.js");
+var doThis = "do-what-it-says";
+var fs = require("fs");
 
-//Twitter Specific Global Variables
+//Twitter Specific Variables
 var Twitter = require("twitter");
 var myTweets = "my-tweets";
 var input = process.argv[2];
 var count = 0;
 
 var twitterKeys = new Twitter({
-    consumer_key: keys.consumer_key,
-    consumer_secret: keys.consumer_secret,
-    access_token_key: keys.access_token_key,
-    access_token_secret: keys.access_token_secret,
+    consumer_key: keys.twitterKeys.consumer_key,
+    consumer_secret: keys.twitterKeys.consumer_secret,
+    access_token_key: keys.twitterKeys.access_token_key,
+    access_token_secret: keys.twitterKeys.access_token_secret,
 });
 // console.log(twitterKeys);
 
 //Twitter My-Tweet Function
-function getTweets() {
+if (input === myTweets) {
+    console.log("here");
     twitterKeys.get("statuses/user_timeline", function(err, tweets, body) {
         // If the request was successful...
         if (!err) {
             // Then log the body from the site!
-            console.log("HEY");
-        }
-
-        if (err) {
-            console.log(err);
-        }
-        if (input === myTweets) {
             for (var i = 0; i < tweets.length; i++) {
                 console.log(tweets[i].text);
                 console.log(tweets[i].created_at);
@@ -37,34 +33,99 @@ function getTweets() {
                     break;
                 }
             }
+
+            if (err) {
+                console.log(err);
+            }
         }
     });
 }
-// getTweets();
-
-// // Spotify Specific Global Variables
+// // Spotify Specific Variables
 var Spotify = require('node-spotify-api');
 var spotifyKey = new Spotify({
-    id: keys.id,
-    secret: keys.secret,
+    id: keys.spotify.id,
+    secret: keys.spotify.secret,
 });
-
-console.log(spotifyKey);
-
 var userSong = process.argv[3];
 var songNameSearch = "spotify-this-song";
 
-//Spotify Function
-spotifyKey.search({ type: "track", query: userSong }, function(err, spotify) {
-    if (err) {
-        return console.log('Error occurred: ' + err);
-    }
-    console.log(userSong);
-    if (input === songNameSearch) {
+function spotifySearch(song) {
+
+    spotifyKey.search({ type: "track", query: song }, function(err, spotify) {
         console.log("SONG NAME: " + spotify.tracks.items[0].name);
         console.log("ARTIST NAME: " + spotify.tracks.items[0].album.artists[0].name);
         console.log("ALBUM: " + spotify.tracks.items[0].album.name);
         console.log("URL: " + spotify.tracks.items[0].album.artists[0].external_urls.spotify);
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+
+    });
+}
+//Spotify Function
+function spotify() {
+    if (input === songNameSearch) {
+        spotifySearch(userSong);
     }
-});
-//Figure out a more efficient way to call twitter functions
+
+    else if (input === doThis) {
+        readFile();
+        console.log("here");
+    }
+}
+spotify();
+
+//OMDB
+var movieThis = "movie-this";
+var userMovie = process.argv[3];
+
+var queryUrl = "http://www.omdbapi.com/?t=" + userMovie + "&y=&plot=short&apikey=trilogy";
+// var queryUrlUndefined = "http://www.omdbapi.com/?t=mr.nobody&y=&plot=short&apikey=trilogy";
+
+if (input === movieThis) {
+    request(queryUrl, function(error, response, body) {
+        if (!error && response.statusCode === 200) {
+            console.log("MOVIE TITLE: " + JSON.parse(body).Title);
+            console.log("RELEASE YEAR: " + JSON.parse(body).Year);
+            console.log("IMDB RATING: " + JSON.parse(body).imdbRating);
+            console.log("ROTTEN TOMATOES RATING: " + JSON.parse(body).Ratings[1].Value);
+            console.log("COUNTRY: " + JSON.parse(body).Country);
+            console.log("LANGUAGE: " + JSON.parse(body).Language);
+            console.log("PLOT: " + JSON.parse(body).Plot);
+            console.log("ACTORS: " + JSON.parse(body).Actors);
+        }
+    });
+}
+// Function to allow for Mr. Nobody
+// if (input === "") {
+//     request(queryUrlUndefined, function(error, response, body) {
+//         if (!error && response.statusCode === 200) {
+//             console.log("MOVIE TITLE: " + JSON.parse(body).Title);
+//             console.log("RELEASE YEAR: " + JSON.parse(body).Year);
+//             console.log("IMDB RATING: " + JSON.parse(body).imdbRating);
+//             console.log("ROTTEN TOMATOES RATING: " + JSON.parse(body).Ratings[1].Value);
+//             console.log("COUNTRY: " + JSON.parse(body).Country);
+//             console.log("LANGUAGE: " + JSON.parse(body).Language);
+//             console.log("PLOT: " + JSON.parse(body).Plot);
+//             console.log("ACTORS " + JSON.parse(body).Actors);
+//             console.log(body);
+//         }
+//     });
+// }
+
+//FS Function
+function readFile() {
+    fs.readFile("random.txt", "utf8", function(err, data) {
+        if (err) {
+            return console.log(err);
+        }
+        if (!err) {
+            var nData = data.split(",");
+            console.log(nData[0], nData[1]);
+            userSong = nData[1];
+            console.log(userSong);
+            songNameSearch = nData[0];
+            spotifySearch(userSong);
+        };
+    });
+};
